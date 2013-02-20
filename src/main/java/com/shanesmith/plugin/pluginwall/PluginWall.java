c529e61149c67cbb914912d746b2476b26624f15
Shane Smith <leiger40+github@gmail.com>
2013-02-21 10:10:20 +1030

    Clean up imports

--- src/main/java/com/shanesmith/plugin/pluginwall/PluginWall.java
+++ src/main/java/com/shanesmith/plugin/pluginwall/PluginWall.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
